b0dbaa5d093fe00ead340afe5af556b359dd1d90
Simone Basso <bassosimone@gmail.com>
2018-10-23 00:40:45 +0200

    Start renaming the library

--- libmmdbx-client.cpp
+++ libmmdbx-client.cpp
@@ -5,18 +5,18 @@
 #define MK_CURLX_INLINE_IMPL
 #include "libcurlx.h"
 
-#define MK_MMDBX_INLINE_IMPL
-#include "libmmdbx.h"
+#define MKGEOIP_INLINE_IMPL
+#include "mkgeoip.h"
 
 int main() {
-  mk_mmdbx_settings_uptr settings{mk_mmdbx_settings_new()};
+  mkgeoip_settings_uptr settings{mkgeoip_settings_new()};
   if (!settings) {
     std::clog << "Out of memory" << std::endl;
     exit(EXIT_FAILURE);
   }
-  mk_mmdbx_settings_set_country_db_path(settings.get(), "country.mmdb");
-  mk_mmdbx_settings_set_asn_db_path(settings.get(), "asn.mmdb");
-  mk_mmdbx_results_uptr results{mk_mmdbx_lookup(settings.get())};
+  mkgeoip_settings_set_country_db_path(settings.get(), "country.mmdb");
+  mkgeoip_settings_set_asn_db_path(settings.get(), "asn.mmdb");
+  mkgeoip_results_uptr results{mkgeoip_lookup(settings.get())};
   if (!results) {
     std::clog << "Out of memory" << std::endl;
     exit(EXIT_FAILURE);
@@ -24,31 +24,31 @@ int main() {
   std::clog << "=== BEGIN SUMMARY ==="
             << std::endl
             << "Error: "
-            << (int)mk_mmdbx_results_get_error(results.get())
+            << (int)mkgeoip_results_get_error(results.get())
             << std::endl
             << "Probe IP: "
-            << mk_mmdbx_results_get_probe_ip(results.get())
+            << mkgeoip_results_get_probe_ip(results.get())
             << std::endl
             << "Probe ASN: "
-            << mk_mmdbx_results_get_probe_asn(results.get())
+            << mkgeoip_results_get_probe_asn(results.get())
             << std::endl
             << "Probe CC: "
-            << mk_mmdbx_results_get_probe_cc(results.get())
+            << mkgeoip_results_get_probe_cc(results.get())
             << std::endl
             << "Probe Network Name: "
-            << mk_mmdbx_results_get_probe_network_name(results.get())
+            << mkgeoip_results_get_probe_network_name(results.get())
             << std::endl
             << "Bytes sent: "
-            << mk_mmdbx_results_get_bytes_sent(results.get())
+            << mkgeoip_results_get_bytes_sent(results.get())
             << std::endl
             << "Bytes recv: "
-            << mk_mmdbx_results_get_bytes_recv(results.get())
+            << mkgeoip_results_get_bytes_recv(results.get())
             << std::endl
             << "=== END SUMMARY ==="
             << std::endl << std::endl;
   std::clog << "=== BEGIN LOGS ==="
             << std::endl
-            << mk_mmdbx_results_get_logs(results.get())
+            << mkgeoip_results_get_logs(results.get())
             << "=== END LOGS ==="
             << std::endl << std::endl;
 }
