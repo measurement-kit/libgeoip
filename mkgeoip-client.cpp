a942988b0ac96d99a2b25c6af3c3e4ba447ebf14
Simone Basso <bassosimone@gmail.com>
2018-10-25 17:15:43 +0200

    Rewrite to minimize bindings surface

--- mkgeoip-client.cpp
+++ mkgeoip-client.cpp
@@ -7,46 +7,43 @@
 #include "mkgeoip.h"
 
 int main() {
-  mkgeoip_settings_uptr settings{mkgeoip_settings_new()};
-  if (!settings) {
+  mkcurl_request_uptr request{mkcurl_request_new()};
+  if (request == nullptr) {
     std::clog << "Out of memory" << std::endl;
     exit(EXIT_FAILURE);
   }
-  mkgeoip_settings_set_country_db_path(settings.get(), "country.mmdb");
-  mkgeoip_settings_set_asn_db_path(settings.get(), "asn.mmdb");
-  mkgeoip_results_uptr results{mkgeoip_lookup(settings.get())};
-  if (!results) {
+  mkgeoip_uptr geoip{mkgeoip_new()};
+  if (geoip == nullptr) {
     std::clog << "Out of memory" << std::endl;
     exit(EXIT_FAILURE);
   }
-  std::clog << "=== BEGIN SUMMARY ==="
-            << std::endl
-            << "Error: "
-            << (int)mkgeoip_results_get_error(results.get())
-            << std::endl
-            << "Probe IP: "
-            << mkgeoip_results_get_probe_ip(results.get())
-            << std::endl
-            << "Probe ASN: "
-            << mkgeoip_results_get_probe_asn(results.get())
-            << std::endl
-            << "Probe CC: "
-            << mkgeoip_results_get_probe_cc(results.get())
-            << std::endl
-            << "Probe Network Name: "
-            << mkgeoip_results_get_probe_network_name(results.get())
-            << std::endl
-            << "Bytes sent: "
-            << mkgeoip_results_get_bytes_sent(results.get())
-            << std::endl
-            << "Bytes recv: "
-            << mkgeoip_results_get_bytes_recv(results.get())
-            << std::endl
-            << "=== END SUMMARY ==="
-            << std::endl << std::endl;
-  std::clog << "=== BEGIN LOGS ==="
-            << std::endl
-            << mkgeoip_results_get_logs(results.get())
-            << "=== END LOGS ==="
-            << std::endl << std::endl;
+  if (!mkgeoip_iplookup(geoip.get(), request.get())) {
+    std::clog << "mkgeoip_iplookup() failed" << std::endl;
+    std::clog << "=== BEGIN LOGS ===" << std::endl;
+    mkcurl_response_uptr response{mkgeoip_get_response(geoip.get())};
+    if (response == nullptr) {
+      std::clog << "Out of memory" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    std::clog << mkcurl_response_get_logs(response.get());
+    std::clog << "=== END LOGS ===" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  const char *probe_ip = mkgeoip_get_probe_ip(geoip.get());
+  std::clog << "Probe IP: " << probe_ip << std::endl;
+  {
+    const char *probe_cc = mkgeoip_mmdb_query_probe_cc(
+        geoip.get(), probe_ip, "country.mmdb");
+    std::clog << "Probe CC: " << probe_cc << std::endl;
+  }
+  {
+    const char *probe_asn = mkgeoip_mmdb_query_probe_asn(
+        geoip.get(), probe_ip, "asn.mmdb");
+    std::clog << "Probe ASN: " << probe_asn << std::endl;
+  }
+  {
+    const char *probe_netname = mkgeoip_mmdb_query_probe_netname(
+        geoip.get(), probe_ip, "asn.mmdb");
+    std::clog << "Probe Network Name: " << probe_netname << std::endl;
+  }
 }
